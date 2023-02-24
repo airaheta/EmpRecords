57bae6d0c534c3b31056e076d1860bbad2c9305f
airaheta <antonioiraheta@gmail.com>
2023-02-24 13:32:04 -0300

    proyect commit

---
+++ EmployeeForm.aspx
@@ -0,0 +1,91 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EmployeeForm.aspx.cs" Inherits="EmployeeRecord.EmployeeForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1
+        {
+            height: 26px;
+        }
+        .auto-style2
+        {
+            height: 30px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+   <div>  
+            <table width="50%">  
+                <tr>  
+                    <td colspan="2" style="background-color: Green; height: 30px;color: White;" align="center">  
+                        Employee Records  
+                    </td>  
+                  </tr>
+                <tr>  
+                    <td class="auto-style2"> ID </td>  
+                    <td class="auto-style2">  
+           <asp:TextBox ID="txtId" Width="150px" runat="server" OnTextChanged="txtId_TextChanged"></asp:TextBox>  
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ErrorMessage="Required " ValidationGroup="valForm1" ControlToValidate="txtId" ForeColor="Red"  ></asp:RequiredFieldValidator>  
+                        <asp:RangeValidator ID="RangeValidator1" runat="server" ErrorMessage="must be whole number higer than 0" MaximumValue="9999" MinimumValue="0" Type="Integer" ControlToValidate="txtId" ValidationGroup="valForm1" ForeColor="Red"></asp:RangeValidator>
+                    
+                    </td>  
+                </tr>  
+                <tr>  
+                    <td class="auto-style1">  
+                        Last Name </td>  
+                    <td class="auto-style1">  
+            <asp:TextBox ID="txtLastName" Width="150px" runat="server"></asp:TextBox>  
+                         </td>
+                </tr>  
+                <tr>  
+                    <td> First Name </td>  
+             <td>  
+            <asp:TextBox ID="txtFirstName" Width="150px" runat="server"></asp:TextBox>  
+             </td>  
+                </tr>  
+                <tr>  
+                    <td> Telephone Number </td>  
+                    <td>  
+            <asp:TextBox ID="txtPhone" Width="150px" runat="server"></asp:TextBox>  
+                    </td>
+                </tr>
+                 <tr>  
+                    <td> Zip </td>  
+                    <td>  
+           <asp:TextBox ID="txtZip" Width="150px" runat="server"></asp:TextBox>  
+                    </td>  
+                </tr>  
+                <tr>  
+                    <td>  
+                        Hire Date </td>  
+                    <td>  
+            <asp:label ID="lblHireDate" Width="150px" runat="server"></asp:label>  
+                    </td>  
+                </tr> 
+                <tr>  
+                    <td align="center" colspan="2">  
+         <asp:Button ID="BtnSave" runat="server" Width="100px" Text="Save" OnClick="BtnSave_Click" style="margin-left: 0px" ValidationGroup="valForm1" />  
+         <asp:Button ID="BtnSave0" runat="server" Width="100px" Text="Delete" OnClick="BtnDelete_Click" ValidationGroup="valForm1" />  
+                    </td>  
+                </tr>  
+                                <tr>  
+                    <td align="center" colspan="2">  
+         <asp:Button ID="Button3" runat="server" Width="203px" Text="Search by Last Name/ Phone" OnClick="BtnFind_Click" Height="19px" style="margin-right: 15px; margin-top: 0px" ValidationGroup="valForm2" />  
+                    </td>  
+                </tr>  
+                    
+            </table>  
+        </div> 
+    </div>
+       <asp:GridView ID="GridView1" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" Width="820px">
+        
+    </asp:GridView>  
+    </form>
+   
+</body>
+</html>
---
+++ EmployeeForm.aspx.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Bussinesslogic;
+using BussinessObject;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Text;
+
+namespace EmployeeRecord
+{
+    public partial class EmployeeForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                FillGrid();
+                ResetForm();
+                lblHireDate.Text = String.Format("{0:dd/MM/yyyy}", DateTime.Now);
+            }
+            
+        }
+        protected void txtId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        protected void BtnSave_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtId.Text)) // Id validation
+            {
+                UserBL BLuser = new UserBL();
+                UserBO BOuser = new UserBO();
+                BOuser.EmployeeID = int.Parse(txtId.Text);
+                BOuser.EmployeeFirstName = txtFirstName.Text;
+                BOuser.EmployeeLastName = txtLastName.Text;
+                BOuser.EmployeePhone = txtPhone.Text;
+                BOuser.EmployeeZip = txtZip.Text;
+                BOuser.HireDate = DateTime.Parse(lblHireDate.Text);
+                BLuser.SaveUserRegisrationBL(BOuser);
+                FillGrid();
+                ResetForm();
+            }
+        }
+        protected void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtId.Text)) // Id validation
+            {
+                UserBL BLuser = new UserBL();
+                UserBO BOuser = new UserBO();
+                BOuser.EmployeeID = int.Parse(txtId.Text);
+                BOuser.EmployeeFirstName = txtFirstName.Text;
+                BOuser.EmployeeLastName = txtLastName.Text;
+                BOuser.EmployeePhone = txtPhone.Text;
+                BOuser.EmployeeZip = txtZip.Text;
+                BOuser.HireDate = DateTime.Parse(lblHireDate.Text);
+                BLuser.DelUserRegisrationBL(BOuser);
+                FillGrid();
+                ResetForm();
+            }
+        }
+        protected void BtnFind_Click(object sender, EventArgs e)
+        {
+            UserBL BLuser = new UserBL();
+            UserBO BOuser = new UserBO();
+            if (String.IsNullOrEmpty(txtLastName.Text) && String.IsNullOrEmpty(txtPhone.Text))
+            {
+                FillGrid();
+            }
+            else {
+
+                if (!String.IsNullOrEmpty(txtLastName.Text))
+                {
+                    BOuser.EmployeeLastName = txtLastName.Text;
+                };
+                if (!String.IsNullOrEmpty(txtPhone.Text))
+                {
+                    BOuser.EmployeePhone = txtPhone.Text;
+                };
+                List<UserBO> userlist = new List<UserBO>();
+                userlist = BLuser.FindUserRegisrationBL(BOuser);
+                if (userlist[0].EmployeeID == -1)
+                {
+                    GridView1.DataSource = new List<UserBO>();
+                }
+                else
+                {
+                    GridView1.DataSource = userlist;
+                } 
+                GridView1.DataBind();
+            }
+        }
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+        protected void ResetForm()
+        {
+            txtId.Text = "";
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtPhone.Text = "";
+            txtZip.Text = "";
+        }
+        protected void FillGrid()
+        {
+            UserBL BLuser = new UserBL();
+            UserBO BOuser = new UserBO();
+            List<UserBO> userlist = new List<UserBO>();
+            BOuser.EmployeeID = -1;
+            userlist = BLuser.FindUserRegisrationBL(BOuser);
+            if (userlist[0].EmployeeID == -1)
+            {
+                GridView1.DataSource = new List<UserBO>();
+            }
+            else
+            {
+                GridView1.DataSource = userlist;
+            }
+            GridView1.DataBind();
+        }
+    }
+}
